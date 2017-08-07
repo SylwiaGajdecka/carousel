db563fbe4a4850a88d604741ce48ff618800d7e5
Sylwia Gajdecka <sylwia.gajdecka@wp.pl>
2017-08-07 16:52:30 +0200

    Add controls to the slider

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ $(function(){
 	var carouselList = $('#js-carousel ul');
 	var basicInterval = setInterval(changeSlide, 3000);
 
+	function setTime(){
+		basicInterval = setInterval(changeSlide, 3000)
+	}
+
 	function changeSlide(){
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide); 
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 	}
 
 	function moveFirstSlide(){
@@ -13,6 +17,7 @@ $(function(){
 
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
+		changeControl(); 
 	}
 
 //Cicrcle left
@@ -23,18 +28,16 @@ $(function(){
 	circleLeft.on('click', movePrevSlide);
 
 	function movePrevSlide(){
-		var firstItem = $('#js-carousel').find('li:first'); //dlaczego kiedy wyciągam te zmienne poza funkcję, slider źle się przewija?
+		var firstItem = $('#js-carousel').find('li:first'); 
 		var lastItem = $('#js-carousel').find('li:last');
 
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft:-400});
 
 		carouselList.animate({'marginLeft':0}, 500, clearInterval(basicInterval));
-		setInterval(changeSlide, 3000);
+		setTime();
+		changeControl(); 
 	}
-
-
-	
 
 //Circle right
 
@@ -43,26 +46,56 @@ $(function(){
 	})
 
 	function moveSlideNext(){
-
-	var firstItem = $('#js-carousel').find('li:first');
-	var lastItem = $('#js-carousel').find('li:last');
+		var firstItem = $('#js-carousel').find('li:first');
+		var lastItem = $('#js-carousel').find('li:last');
 
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 		clearInterval(basicInterval);
-		setInterval(changeSlide, 3000);
+		setTime();
+		changeControl(); 
 	}
+//Kontrolki 
+	var controls = $('.js-controlItem');
+	var slide = $('.picture');
 
-//Kontrolki 1. Gdy jest slide[0], li kontrolki [0] ma być niebieski. Gdy kolejny slajd - kolejna kontrolka 
+	controls.each(function(index, element){ //przypisanine data- z nr indeksów do kontrolek
+		$(element).data('control', index);
+	})
 
-	function changeControlColor(){
+	slide.each(function(index, element){ //przypisanie data- z nr indeksów do slidów
+		$(element).data('slide', index);
+	})
 
-		var firstControl = $('controls ul').find('li:first').css('background', 'blue');
-		var coloredControl = firstControl.eq(0);
-		
-	}	
-	
-	function changeSlideWithControlItem(){
-		
-	}
+	function changeControl() {
+        var dataSlide = $('li').first().data('slide'); //wyciągam index slajdu pierwszego
+        var control = controls.eq(dataSlide);
+
+        console.log(dataSlide);
+
+        controls.css({'background': 'rgba(255, 255, 255, .5)'}); //koloruję wszystkie kółka
+        control.css('background', 'white'); //koloruję kółko o indeksie równym indeksowi slidu
+ 	}
+ //Przypisanie handlerów na kontrolki
+ 	$('li').click(function(){
+ 		controls.css({'background': 'rgba(255, 255, 255, .5)'});
+ 		$(this).css('background', 'white');  //naciśnięcie kontrolki - zmiana jej koloru
+ 		var activeControlIndex = $(this).index();
+
+ 		moveSlideControl(activeControlIndex);
+ 	})
+ //1. naciśnięcie kontrolki - zmiana slidu: naciskam kontrolkę nr 0 - zmienia się slajd na nr 0
+ 	function moveSlideControl(activeControlIndex){
+		var firstItem = $('#js-carousel').find('li:first'); 
+		var lastItem = $('#js-carousel').find('li:last');
+
+		console.log(activeControlIndex);
+
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-400*activeControlIndex});
+
+		carouselList.animate({'marginLeft':0}, 500, clearInterval(basicInterval));
+		setTime();
+		changeControl(); 
+ 	}
 })
